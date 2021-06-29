3279ff07e492341da15149c00c30e14e34ae5f07
ruleona <61205714+ruleona@users.noreply.github.com>
2021-06-29 16:59:41 +0300

    update Вложенные_списки.py

--- Вложенные_списки.py
+++ Вложенные_списки.py
@@ -26,19 +26,45 @@
 #
 # print(maximum + minimum)
 
-#              Треугольник Паскаля
-def pascal(n):
-    list1 = []
-    for i in range(n + 1):
-        temp_list = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                temp_list.append(1)
-            else:
-                temp_list.append(list1[i-1][j] + list1[i-1][j-1])
-        list1.append(temp_list)
-        print(temp_list)
-    return list1[n]
+# #              Треугольник Паскаля
+# def pascal(n):
+#     list1 = []
+#     for i in range(n + 1):
+#         temp_list = []
+#         for j in range(i + 1):
+#             if j == 0 or j == i:
+#                 temp_list.append(1)
+#             else:
+#                 temp_list.append(list1[i-1][j] + list1[i-1][j-1])
+#         list1.append(temp_list)
+#         print(temp_list)
+#     return list1[n]
+#
+# row = int(input())
+# print(pascal(row))
 
-row = int(input())
-print(pascal(row))
+# #                 Chunked
+# def chunked(lst, n):
+#     new = []
+#     for i in range(len(lst)):
+#         if i % n == 0:
+#             new.append([lst[i]])
+#         else:
+#             new[-1].append(lst[i])
+#     return new
+#
+# list_for_chunk = input().split()
+# print(chunked(list_for_chunk, int(input())))
+
+#                   Упаковка дубликатов
+# list1 = input().split()
+# result = []
+# for i in range(len(list1)):
+#     if i == 0:
+#         result.append([list1[0]])
+#     else:
+#         if list1[i] == list1[i - 1]:
+#             result[-1].append(list1[i])
+#         else:
+#             result.append([list1[i]])
+# print(result)
