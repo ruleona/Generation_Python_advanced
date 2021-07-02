631652bf86084ae06acc30b142a338162daa35e7
ruleona <61205714+ruleona@users.noreply.github.com>
2021-07-02 17:29:02 +0300

    update Матрицы_2_часть.py

--- Матрицы_2_часть.py
+++ Матрицы_2_часть.py
@@ -39,3 +39,37 @@
 #         print(matrix[r][c], end=' ')
 #     print()
 
+#                   Симметричность матрицы(первый вариант)
+# n = int(input())
+# matrix = [[int(i) for i in input().split()] for _ in range(n)]
+# flag = True
+# for i in range(n):
+#     if flag:
+#         for j in range(len(matrix[i])):
+#             if i > j and matrix[i][j] != matrix[j][i]:
+#                 flag = False
+#                 break
+# print('YES' if flag else 'NO')
+
+#                  Симметричность матрицы(второй вариант с all)
+# n = int(input())
+# matrix = [[int(j) for j in input().split()] for _ in range(n)]
+# print('YES' if all([matrix[i][j] == matrix[j][i] for j in range(n) for i in range(n)]) else 'NO')
+
+#                  Обмен диагоналей
+# n = int(input())
+# matrix = [[int(i) for i in input().split()] for _ in range(n)]
+# for i in range(n):
+#     for j in range(n):
+#         if j == i:
+#             matrix[i][j], matrix[n - i - 1][j] = matrix[n - i - 1][j], matrix[i][j]
+# for i in range(n):
+#     print(*matrix[i])
+
+#                  Зеркальное отображение
+# n = int(input())
+# matrix = [[int(i) for i in input().split()] for _ in range(n)]
+# for i in range(int(n / 2)):
+#     matrix[i], matrix[n - i - 1] = matrix[n - i - 1], matrix[i]
+# for i in range(n):
+#     print(*matrix[i])
