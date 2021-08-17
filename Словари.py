02450363074f9b610cbd903a5fbffe86898341c5
ruleona <61205714+ruleona@users.noreply.github.com>
2021-08-17 16:31:41 +0300

    update Словари.py

--- Словари.py
+++ Словари.py
@@ -174,3 +174,19 @@
 # for _ in range(int(input())):
 #     name = input()
 #     print((dct.get(name)) if name in dct else 'абонент не найден')
+
+
+
+
+
+# student_ids = ['S001', 'S002', 'S003', 'S004', 'S005', 'S006', 'S007', 'S008', 'S009', 'S010', 'S011', 'S012', 'S013']
+# student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', 'Batista Cesare', 'Francesco Totti', 'Khalid Hussain', 'Ethan Hawke', 'David Bowman', 'James Milner', 'Michael Owen', 'Gary Oldman', 'Tom Hardy']
+# student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
+# result = []
+# for i in range(len(student_ids)):
+#     item = dict({student_ids[i]: dict({student_names[i]: student_grades[i]})})
+#     result.append(item)
+
+#Решение Алексея Халабудина:
+# result = [{x: {y: z}} for x, y, z in zip(student_ids, student_names, student_grades)]
+
